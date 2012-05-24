78918fc4e547bb008e1cff0ee308bf6db0753eba
Robert Lindley <robert.lindley@webappuk.com>
2012-05-24 12:47:18 -0100

    Case:16223 Added financial class to the core utilities library. This object binds a decimal and currency together into one object.

---
+++ Utilities/Financials/Financial.cs
@@ -0,0 +1,215 @@
+﻿using System;
+
+namespace WebApplications.Utilities.Financials
+{
+    /// <summary>
+    /// Represents a financial value and currency binding.
+    /// </summary>
+    public class Financial : ICloneable, IEquatable<Financial>
+    {
+        /// <summary>
+        /// Gets or sets the currency.
+        /// </summary>
+        /// <value>
+        /// The currency.
+        /// </value>
+        public CurrencyInfo Currency { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the amount.
+        /// </summary>
+        /// <value>
+        /// The amount.
+        /// </value>
+        public decimal Amount { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Financial"/> class.
+        /// </summary>
+        /// <param name="currency">The currency.</param>
+        /// <param name="amount"> </param>
+        public Financial(CurrencyInfo currency, decimal amount)
+        {
+            Currency = currency;
+            Amount = amount;
+        }
+
+        /// <summary>
+        /// Implements the operator +.
+        /// </summary>
+        /// <param name="a">A.</param>
+        /// <param name="b">The b.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static Financial operator +(Financial a, Financial b)
+        {
+            ValidateCurrenciesMatch(a.Currency, b.Currency, "Addition");
+            return new Financial(a.Currency, a.Amount + b.Amount);
+        }
+
+        /// <summary>
+        /// Implements the operator +.
+        /// </summary>
+        /// <param name="financial">The financial.</param>
+        /// <param name="amount">The amount.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static Financial operator +(Financial financial, decimal amount)
+        {
+            Financial clone = (Financial)financial.Clone();
+            clone.Amount = financial.Amount + amount;
+            return clone;
+        }
+
+        /// <summary>
+        /// Implements the operator -.
+        /// </summary>
+        /// <param name="a">A.</param>
+        /// <param name="b">The b.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static Financial operator -(Financial a, Financial b)
+        {
+            ValidateCurrenciesMatch(a.Currency, b.Currency, "Subtraction");
+            return new Financial(a.Currency, a.Amount - b.Amount);
+        }
+
+        /// <summary>
+        /// Implements the operator -.
+        /// </summary>
+        /// <param name="financial">The financial.</param>
+        /// <param name="amount">The amount.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static Financial operator -(Financial financial, decimal amount)
+        {
+            Financial clone = (Financial)financial.Clone();
+            clone.Amount = financial.Amount - amount;
+            return clone;
+        }
+
+        /// <summary>
+        /// Implements the operator &lt;.
+        /// </summary>
+        /// <param name="a">A.</param>
+        /// <param name="b">The b.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static bool operator <(Financial a, Financial b)
+        {
+            ValidateCurrenciesMatch(a.Currency, b.Currency, "less than");
+            return a.Amount < b.Amount;
+        }
+
+        /// <summary>
+        /// Implements the operator &gt;.
+        /// </summary>
+        /// <param name="a">A.</param>
+        /// <param name="b">The b.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static bool operator >(Financial a, Financial b)
+        {
+            ValidateCurrenciesMatch(a.Currency, b.Currency, "more than");
+            return a.Amount > b.Amount;
+        }
+
+        /// <summary>
+        /// Implements the operator &lt;=.
+        /// </summary>
+        /// <param name="a">A.</param>
+        /// <param name="b">The b.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static bool operator <=(Financial a, Financial b)
+        {
+            ValidateCurrenciesMatch(a.Currency, b.Currency, "less than or equal to");
+            return a.Amount <= b.Amount;
+        }
+
+        /// <summary>
+        /// Implements the operator &gt;=.
+        /// </summary>
+        /// <param name="a">A.</param>
+        /// <param name="b">The b.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static bool operator >=(Financial a, Financial b)
+        {
+            ValidateCurrenciesMatch(a.Currency, b.Currency, "more than or equal to");
+            return a.Amount >= b.Amount;
+        }
+
+        /// <summary>
+        /// Implements the operator *.
+        /// </summary>
+        /// <param name="a">A.</param>
+        /// <param name="b">The b.</param>
+        /// <returns>
+        /// The result of the operator.
+        /// </returns>
+        public static Financial operator *(Financial a, Financial b)
+        {
+            return new Financial(a.Currency, a.Amount * b.Amount);
+        }
+
+        /// <summary>
+        /// Validates the currencies match.
+        /// </summary>
+        /// <param name="a">A.</param>
+        /// <param name="b">The b.</param>
+        /// <param name="operation">The operation.</param>
+        private static void ValidateCurrenciesMatch(CurrencyInfo a, CurrencyInfo b, string operation)
+        {
+            if (a != b)
+                throw new InvalidOperationException(
+                    string.Format(
+                        "The currency of the first operand '{0}' did not match that of the second '{1}' during an {2} operation.",
+                        a,
+                        b,
+                        operation));
+        }
+
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns>
+        /// true if the current object is equal to the other parameter; otherwise, false.
+        /// </returns>
+        public bool Equals(Financial other)
+        {
+            return other != null && Currency == other.Currency && Amount == other.Amount;
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String"/> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return string.Format("Financial {0}{1}", Amount, Currency.Code);
+        }
+
+        /// <summary>
+        /// Creates a new object that is a copy of the current instance.
+        /// </summary>
+        /// <returns>
+        /// A new object that is a copy of this instance.
+        /// </returns>
+        public object Clone()
+        {
+            return new Financial(Currency, Amount);
+        }
+    }
+}
---
+++ Utilities/WebApplications.Utilities.Test/FinancialsTests.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplications.Testing;
+using WebApplications.Utilities.Financials;
+
+namespace WebApplications.Utilities.Test
+{
+    [TestClass]
+    public class FinancialsTests : UtilitiesTestBase
+    {
+        private readonly CurrencyInfo _currency = CurrencyInfo.Get("GBP");
+        private readonly decimal _amount = Random.RandomDecimal();
+
+        [TestMethod]
+        public void TestCanCreateInstance()
+        {
+            Financial financial = new Financial(null, 0);
+            Assert.IsNotNull(financial);
+        }
+
+        [TestMethod]
+        public void TestConstructorSetsCurrencyInfoPropertyToExpectedValue()
+        {
+            Financial financial = new Financial(_currency, 0);
+            Assert.IsNotNull(financial.Currency);
+            Assert.AreEqual("GBP", financial.Currency.Code);
+        }
+
+        [TestMethod]
+        public void TestConstructorSetsAmountPropertyToExpectedValue()
+        {
+            Financial financial = new Financial(_currency, _amount);
+            Assert.IsNotNull(financial.Currency);
+            Assert.AreEqual(_amount, financial.Amount);
+        }
+
+        [TestMethod]
+        public void TestToStringReturnsExpectedValue()
+        {
+            Financial financial = new Financial(_currency, _amount);
+            Assert.IsNotNull(financial.Currency);
+            Assert.AreEqual(string.Format("Financial {0}{1}", _amount, _currency.Code), financial.ToString());
+        }
+
+        [TestMethod]
+        public void TestAddOperatorAddsTheAmountsInTheFinancials()
+        {
+            decimal amountA = Random.RandomDecimal();
+            decimal amountB = Random.RandomDecimal();
+
+            Financial financialA = new Financial(_currency, amountA);
+            Financial financialB = new Financial(_currency, amountB);
+
+            Financial result = financialA + financialB;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(amountA + amountB, result.Amount);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestAddOperatorThrowsExceptionIfTheCurrenciesAreDifferent()
+        {
+            Financial financialA = new Financial(CurrencyInfo.Get("GBP"), 0);
+            Financial financialB = new Financial(CurrencyInfo.Get("USD"), 0);
+            Financial result = financialA + financialB;
+        }
+
+        [TestMethod]
+        public void TestSubtractOperatorSubtractsTheAmountsInTheFinancials()
+        {
+            decimal amountA = Random.RandomDecimal();
+            decimal amountB = Random.RandomDecimal();
+
+            Financial financialA = new Financial(_currency, amountA);
+            Financial financialB = new Financial(_currency, amountB);
+
+            Financial result = financialA - financialB;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(amountA - amountB, result.Amount);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestSubtractOperatorThrowsExceptionIfTheCurrenciesAreDifferent()
+        {
+            Financial financialA = new Financial(CurrencyInfo.Get("GBP"), 0);
+            Financial financialB = new Financial(CurrencyInfo.Get("USD"), 0);
+            Financial result = financialA - financialB;
+        }
+
+        [TestMethod]
+        public void TestSubtractOperatorForDecimalSubtractionsReturnsCloneWithCorrectAmount()
+        {
+            decimal amountA = Random.RandomDecimal();
+            decimal amountB = Random.RandomDecimal();
+
+            Financial financialA = new Financial(_currency, amountA);
+            Financial result = financialA - amountB;
+            Assert.AreEqual(amountA - amountB, result.Amount);
+            Assert.AreNotSame(financialA, result);
+        }
+
+        [TestMethod]
+        public void TestAddOperatorForDecimalAdditionsReturnsCloneWithCorrectAmount()
+        {
+            decimal amountA = Random.RandomDecimal();
+            decimal amountB = Random.RandomDecimal();
+
+            Financial financialA = new Financial(_currency, amountA);
+            Financial result = financialA + amountB;
+            Assert.AreEqual(amountA + amountB, result.Amount);
+            Assert.AreNotSame(financialA, result);
+        }
+
+        [TestMethod]
+        public void TestLessThanOperatorReturnsTheExpectedValue()
+        {
+            decimal amountA = Random.RandomDecimal();
+            decimal amountB = Random.RandomDecimal();
+            bool calcResult = amountA < amountB;
+
+            Financial financialA = new Financial(_currency, amountA);
+            Financial financialB = new Financial(_currency, amountB);
+
+            bool result = financialA < financialB;
+            Assert.AreEqual(calcResult, result);
+        }
+
+        [TestMethod]
+        public void TestMoreThanOperatorReturnsTheExpectedValue()
+        {
+            decimal amountA = Random.RandomDecimal();
+            decimal amountB = Random.RandomDecimal();
+            bool calcResult = amountA > amountB;
+
+            Financial financialA = new Financial(_currency, amountA);
+            Financial financialB = new Financial(_currency, amountB);
+
+            bool result = financialA > financialB;
+            Assert.AreEqual(calcResult, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestLessThanOperatorThrowsExceptionIfTheCurrenciesAreDifferent()
+        {
+            Financial financialA = new Financial(CurrencyInfo.Get("GBP"), 0);
+            Financial financialB = new Financial(CurrencyInfo.Get("USD"), 0);
+            bool result = financialA < financialB;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestMoreThanOperatorThrowsExceptionIfTheCurrenciesAreDifferent()
+        {
+            Financial financialA = new Financial(CurrencyInfo.Get("GBP"), 0);
+            Financial financialB = new Financial(CurrencyInfo.Get("USD"), 0);
+            bool result = financialA > financialB;
+        }
+
+        [TestMethod]
+        public void TestLessThanOrEqualToOperatorReturnsTheExpectedValue()
+        {
+            decimal amountA = Random.RandomDecimal();
+            decimal amountB = Random.RandomDecimal();
+            bool calcResult = amountA <= amountB;
+
+            Financial financialA = new Financial(_currency, amountA);
+            Financial financialB = new Financial(_currency, amountB);
+
+            bool result = financialA <= financialB;
+            Assert.AreEqual(calcResult, result);
+        }
+
+        [TestMethod]
+        public void TestMoreThanOrEqualToOperatorReturnsTheExpectedValue()
+        {
+            decimal amountA = Random.RandomDecimal();
+            decimal amountB = Random.RandomDecimal();
+            bool calcResult = amountA >= amountB;
+
+            Financial financialA = new Financial(_currency, amountA);
+            Financial financialB = new Financial(_currency, amountB);
+
+            bool result = financialA >= financialB;
+            Assert.AreEqual(calcResult, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestLessThanOrEqualToOperatorThrowsExceptionIfTheCurrenciesAreDifferent()
+        {
+            Financial financialA = new Financial(CurrencyInfo.Get("GBP"), 0);
+            Financial financialB = new Financial(CurrencyInfo.Get("USD"), 0);
+            bool result = financialA <= financialB;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestMoreThanOrEqualToOperatorThrowsExceptionIfTheCurrenciesAreDifferent()
+        {
+            Financial financialA = new Financial(CurrencyInfo.Get("GBP"), 0);
+            Financial financialB = new Financial(CurrencyInfo.Get("USD"), 0);
+            bool result = financialA >= financialB;
+        }
+
+        [TestMethod]
+        public void TestMultiplyOperatorReturnsTheExpectedValue()
+        {
+            decimal amountA = Random.Next();
+            decimal amountB = Random.Next();
+
+            Financial financialA = new Financial(_currency, amountA);
+            Financial financialB = new Financial(_currency, amountB);
+
+            Financial result = financialA * financialB;
+            Assert.AreEqual(amountA*amountB, result.Amount);
+        }
+
+        [TestMethod]
+        public void TestEqualsComparerReturnsTrueWhenFinancialsAreEqual()
+        {
+            Financial financialA = new Financial(_currency, 10);
+            Financial financialB = new Financial(_currency, 10);
+
+            bool result = financialA.Equals(financialB);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestEqualityComparerReturnsFalseWhenFinancialsAreNotEqual()
+        {
+            Financial financialA = new Financial(_currency, 10);
+            Financial financialB = new Financial(_currency, 15);
+
+            bool result = financialA == financialB;
+            Assert.IsFalse(result);
+        }
+    }
+}
