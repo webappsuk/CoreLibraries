9f385fdf8e5b4d1c4c5375c17ac8a7141a04b840
Robert Lindley <robert.lindley@webappuk.com>
2012-06-07 14:18:18 -0100

    Case:16223 Added FinancialRange.cs

---
+++ Utilities/Ranges/FinancialRange.cs
@@ -0,0 +1,17 @@
+﻿using WebApplications.Utilities.Financials;
+
+namespace WebApplications.Utilities.Ranges
+{
+    public class FinancialRange : Range<Financial>
+    {
+        public FinancialRange(Financial start, Financial end) 
+            : base(start, end)
+        { }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return string.Format("{0} - {1}", Start, End);
+        }
+    }
+}
