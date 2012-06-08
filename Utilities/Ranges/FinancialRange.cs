384c8915411d7da740a4c4770397fe0f386c7339
Robert Lindley <robert.lindley@webappuk.com>
2012-06-08 09:37:18 -0100

    Case:16223 Added comments, contracts and annotations. Also added a step parameter as a decimal becuase the amount will be stepped through and it just didn't make sense for the step to be a Financial instance.

--- Utilities/Ranges/FinancialRange.cs
+++ Utilities/Ranges/FinancialRange.cs
@@ -1,12 +1,45 @@
-﻿using WebApplications.Utilities.Financials;
+﻿using System.Diagnostics.Contracts;
+using JetBrains.Annotations;
+using WebApplications.Utilities.Financials;
 
 namespace WebApplications.Utilities.Ranges
 {
+    /// <summary>
+    /// A range of <see cref="Financial"/> financial.
+    /// </summary>
+    /// <remarks></remarks>
     public class FinancialRange : Range<Financial>
     {
-        public FinancialRange(Financial start, Financial end) 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Range&lt;T&gt;" /> class.
+        /// </summary>
+        /// <param name="start">The start value (inclusive).</param>
+        /// <param name="end">The end value (inclusive).</param>
+        /// <exception cref="System.ArgumentOutOfRangeException">
+        /// The <paramref name="start" /> value was greater than the <paramref name="end" /> value.
+        ///   </exception>
+        /// <remarks></remarks>
+        public FinancialRange([NotNull]Financial start, [NotNull]Financial end) 
             : base(start, end)
-        { }
+        {
+            Contract.Requires(start.Currency == end.Currency, "The currencies in the financial parameters must match.");
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Range&lt;T&gt;" /> class.
+        /// </summary>
+        /// <param name="start">The start value.</param>
+        /// <param name="end">The end value (inclusive).</param>
+        /// <param name="step">The range step (inclusive).</param>
+        /// <exception cref="System.ArgumentOutOfRangeException">
+        /// The <paramref name="start" /> value was greater than the <paramref name="end" /> value.
+        ///   </exception>
+        /// <remarks></remarks>
+        public FinancialRange([NotNull]Financial start, [NotNull]Financial end, decimal step)
+            : base(start, end, new Financial(start.Currency, step))
+        {
+            Contract.Requires(start.Currency == end.Currency, "The currencies in the financial parameters must match.");
+        }
 
         /// <inheritdoc/>
         public override string ToString()
