b4f41f237186035b1cd17d49b05fad90aefd3d3c
Emma Maloney <emma.maloney@webappuk.com>
2012-09-07 15:48:03 -0100

    Case:25418 Upgrading Logging to .NET 4.5
    - Assembly version has been changed to 2.0.*.
    - Target Framework has been changed to 4.5.
    - NuGets this depends on have been upgraded and installed.
    - Code has been cleaned up using ReSharper.

--- Performance/PerformanceTimerHelper.cs
+++ Performance/PerformanceTimerHelper.cs
@@ -1,23 +1,28 @@
-﻿#region © Copyright Web Applications (UK) Ltd, 2011.  All rights reserved.
-// Solution: WebApplications.Utilities.Logging 
-// Project: WebApplications.Utilities.Logging
-// File: PerformanceTimerHelper.cs
+﻿#region © Copyright Web Applications (UK) Ltd, 2012.  All rights reserved.
+// Copyright (c) 2012, Web Applications UK Ltd
+// All rights reserved.
 // 
-// This software, its object code and source code and all modifications made to
-// the same (the “Software”) are, and shall at all times remain, the proprietary
-// information and intellectual property rights of Web Applications (UK) Limited. 
-// You are only entitled to use the Software as expressly permitted by Web
-// Applications (UK) Limited within the Software Customisation and
-// Licence Agreement (the “Agreement”).  Any copying, modification, decompiling,
-// distribution, licensing, sale, transfer or other use of the Software other than
-// as expressly permitted in the Agreement is expressly forbidden.  Web
-// Applications (UK) Limited reserves its rights to take action against you and
-// your employer in accordance with its contractual and common law rights
-// (including injunctive relief) should you breach the terms of the Agreement or
-// otherwise infringe its copyright or other intellectual property rights in the
-// Software.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of Web Applications UK Ltd nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
 // 
-// © Copyright Web Applications (UK) Ltd, 2011.  All rights reserved.
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL WEB APPLICATIONS UK LTD BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endregion
 
 using System;
@@ -41,21 +46,17 @@ namespace WebApplications.Utilities.Logging.Performance
         /// <summary>
         ///   The timer counter category name.
         /// </summary>
-        [NotNull]
-        [UsedImplicitly]
-        public readonly string CategoryName;
+        [NotNull] [UsedImplicitly] public readonly string CategoryName;
 
         /// <summary>
         ///   The length of <see cref="TimeSpan">time</see> for an operation before logging an error.
         /// </summary>
-        [UsedImplicitly]
-        public readonly TimeSpan CriticalTimeSpan;
+        [UsedImplicitly] public readonly TimeSpan CriticalTimeSpan;
 
         /// <summary>
         ///   The length of <see cref="TimeSpan">time</see> for an operation before logging a warning.
         /// </summary>
-        [UsedImplicitly]
-        public readonly TimeSpan WarningTimeSpan;
+        [UsedImplicitly] public readonly TimeSpan WarningTimeSpan;
 
         /// <summary>
         ///   Used to lock initialisation.
@@ -86,7 +87,8 @@ namespace WebApplications.Utilities.Logging.Performance
         /// <param name="criticalTimeSpan">
         ///   The length of time that the operation should take before logging an error.
         /// </param>
-        private PerformanceTimerHelper([NotNull]string categoryName, TimeSpan warningTimeSpan, TimeSpan criticalTimeSpan)
+        private PerformanceTimerHelper([NotNull] string categoryName, TimeSpan warningTimeSpan,
+                                       TimeSpan criticalTimeSpan)
         {
             CategoryName = categoryName;
             WarningTimeSpan = warningTimeSpan;
@@ -198,9 +200,9 @@ namespace WebApplications.Utilities.Logging.Performance
                     _found = false;
                     // Create error but don't throw.
                     new LoggingException(exception,
-                        Resources.PerformanceTimerHelper_Initialize_UnhandledExceptionOccurred,
-                        LogLevel.SystemNotification,
-                        CategoryName);
+                                         Resources.PerformanceTimerHelper_Initialize_UnhandledExceptionOccurred,
+                                         LogLevel.SystemNotification,
+                                         CategoryName);
                 }
             }
         }
@@ -219,7 +221,8 @@ namespace WebApplications.Utilities.Logging.Performance
         ///   The length of time that the operation should take before logging an error.
         /// </param>
         /// <returns>The added/retrieved timer with the <paramref name="categoryName"/> specified.</returns>
-        public static PerformanceTimerHelper Get([NotNull]string categoryName, TimeSpan warningTimeSpan, TimeSpan criticalTimeSpan)
+        public static PerformanceTimerHelper Get([NotNull] string categoryName, TimeSpan warningTimeSpan,
+                                                 TimeSpan criticalTimeSpan)
         {
             PerformanceTimerHelper timerHelper = _counters.GetOrAdd(
                 categoryName, n => new PerformanceTimerHelper(n, warningTimeSpan, criticalTimeSpan));
@@ -243,7 +246,7 @@ namespace WebApplications.Utilities.Logging.Performance
             _totalOperationsPerSecond.Increment();
 
             // Get the duration in CPU ticks rather than DateTime ticks.
-            _averageOperationDuration.IncrementBy((duration.Ticks * Stopwatch.Frequency) / 10000000);
+            _averageOperationDuration.IncrementBy((duration.Ticks*Stopwatch.Frequency)/10000000);
             _averageOperationDurationBase.Increment();
             if (duration < WarningTimeSpan)
                 return;
