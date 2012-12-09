c64feb29635e53b37add326be3b9436a2d64feff
Craig Dean <craig.dean@webappuk.com>
2012-12-09 19:52:09 +0000

    Case:16223 Added PerfSetup builder, which is a command line tool for finding and adding/listing/deleting performance counters.
    Use:
    PerfSetup.exe /?
    to get help.

---
+++ Performance/PerformanceCounterExtensions.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
+namespace WebApplications.Utilities.Logging.Performance
+{
+    /// <summary>
+    /// Extensions for performance counters.
+    /// </summary>
+    public static class PerformanceCounterExtensions
+    {
+        /// <summary>
+        /// Returns all the missing performance counters.
+        /// </summary>
+        /// <param name="performanceCounters">The performance counters.</param>
+        /// <returns>An enumeration of any missing performance counters.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<PerformanceInformation> Missing([NotNull]
+            this IEnumerable<PerformanceInformation> performanceCounters)
+        {
+            return performanceCounters.Where(i => !i.Exists);
+        }
+
+        /// <summary>
+        /// Whether all the performance counters exist.
+        /// </summary>
+        /// <param name="performanceCounters">The performance counters.</param>
+        /// <returns><c>true</c> if all the counters exist fully, <c>false</c> otherwise.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool Exists([NotNull]
+            this IEnumerable<PerformanceInformation> performanceCounters)
+        {
+            return !Missing(performanceCounters).Any();
+        }
+    }
+}
---
+++ Performance/WebApplications.Utilities.Logging.Tools.PerfSetup/Options.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CmdLine;
+
+namespace WebApplications.Utilities.Logging.Tools.PerfSetup
+{
+    /// <summary>
+    /// The applications command line options.
+    /// </summary>
+    [CommandLineArguments(Program = "PerfSetup", Title = "Web Applications Performance Setup",
+        Description = "Provides utilities for interacting with Web Applications Performance Counters.")]
+    internal class Options
+    {
+        [CommandLineParameter(Command = "?", Default = false, Description = "Show Help", Name = "Help", IsHelp = true)]
+        public bool Help { get; set; }
+
+        [CommandLineParameter(Command = "M", Default = ".", Description = "Machine Name", Name = "MachineName")]
+        public string MachineName { get; set; }
+
+        [CommandLineParameter(Name = "Mode", ParameterIndex = 1, Required = true, Description =
+            @"Set the mode to one of the following:
+Add - Add performance counters (requires run as administrator).
+Delete - Delete performance counters (requires run as administrator).
+List - Lists performance counters.")]
+        public string Mode { get; set; }
+
+        [CommandLineParameter(Name = "path", ParameterIndex = 2, Required = true, Description = "Specifies the file or files to scan, you can use wildcards or folders.")]
+        public string Path { get; set; }
+    }
+}
