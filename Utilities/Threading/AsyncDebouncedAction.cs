7e177bf58fad90f4c65db27096be3a8b810aee9c
Craig Dean <craig.dean@webappuk.com>
2014-03-27 21:42:36 +0000

    Case:32794 Split AsyncDebouncedFunction into Function and Action, and added duration and minimumGap.

--- Utilities/Threading/AsyncDebouncedAction.cs
+++ Utilities/Threading/AsyncDebouncedAction.cs
@@ -1,4 +1,4 @@
-#region � Copyright Web Applications (UK) Ltd, 2013.  All rights reserved.
+﻿#region © Copyright Web Applications (UK) Ltd, 2013.  All rights reserved.
 // Copyright (c) 2013, Web Applications UK Ltd
 // All rights reserved.
 // 
@@ -25,104 +25,99 @@
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endregion
 
+using JetBrains.Annotations;
 using System;
 using System.Diagnostics.Contracts;
 using System.Threading;
 using System.Threading.Tasks;
-using JetBrains.Annotations;
 
 namespace WebApplications.Utilities.Threading
 {
     /// <summary>
-    /// Debounces an asynchronous function.
+    /// Debounces an asynchronous action.
     /// </summary>
-    public class AsyncDebouncedFunction<TResult>
+    public class AsyncDebouncedAction
     {
         /// <summary>
-        /// The semaphore controls concurrent access to function execution.
+        /// The semaphore controls concurrent access to action execution.
         /// </summary>
         [NotNull]
         private readonly AsyncLock _lock = new AsyncLock();
 
         /// <summary>
-        /// The asynchronous function to run.
+        /// The asynchronous action to run.
         /// </summary>
         [NotNull]
-        private readonly Func<CancellationToken, Task<TResult>> _function;
+        private readonly Func<CancellationToken, Task> _action;
 
         /// <summary>
-        /// The duration ticks is the number of ticks to leave after a successfully completed function invocation, from the start of the last successful invocation.
+        /// The duration ticks is the number of ticks to leave after a successfully completed action invocation, from the start of the last successful invocation.
         /// </summary>
         private readonly long _durationTicks;
 
         /// <summary>
-        /// The gap ticks is the number of ticks to leave after a successfully completed function invocation, from the end of the last successful invocation.
+        /// The gap ticks is the number of ticks to leave after a successfully completed action invocation, from the end of the last successful invocation.
         /// </summary>
         private readonly long _minimumGapTicks;
 
         /// <summary>
-        /// The next run is the earliest tick that the function can be run again.
+        /// The next run is the earliest tick that the action can be run again.
         /// </summary>
         private long _nextRun;
 
         /// <summary>
-        /// The last result.
+        /// Initializes a new instance of the <see cref="AsyncDebouncedAction" /> class.
         /// </summary>
-        private TResult _lastResult;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="AsyncDebouncedFunction{T}" /> class.
-        /// </summary>
-        /// <param name="function">The function.</param>
+        /// <param name="action">The action.</param>
         /// <param name="duration">The duration is the amount of time the result of a successful execution is held, after the point a successful request was made.</param>
-        /// <param name="minimumGap">The minimum gap, is the time left after a successful execution before the function can be run again.</param>
+        /// <param name="minimumGap">The minimum gap, is the time left after a successful execution before the action can be run again.</param>
         [PublicAPI]
-        public AsyncDebouncedFunction(
-            [NotNull] Func<Task<TResult>> function,
+        public AsyncDebouncedAction(
+            [NotNull] Func<Task> action,
             TimeSpan duration = default(TimeSpan),
             TimeSpan minimumGap = default(TimeSpan))
-            : this(token => function(), duration, minimumGap)
+            : this(token => action(), duration, minimumGap)
         {
-            Contract.Requires(function != null);
+            Contract.Requires(action != null);
             Contract.Requires(duration >= TimeSpan.Zero);
             Contract.Requires(minimumGap >= TimeSpan.Zero);
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="AsyncDebouncedFunction{T}"/> class.
+        /// Initializes a new instance of the <see cref="AsyncDebouncedAction"/> class.
         /// </summary>
-        /// <param name="function">The function.</param>
+        /// <param name="action">The action.</param>
         /// <param name="duration">The duration is the amount of time the result of a successful execution is held, after the point a successful request was made.</param>
-        /// <param name="minimumGap">The minimum gap, is the time left after a successful execution before the function can be run again.</param>
+        /// <param name="minimumGap">The minimum gap, is the time left after a successful execution before the action can be run again.</param>
         [PublicAPI]
-        public AsyncDebouncedFunction(
-            [NotNull]Func<CancellationToken, Task<TResult>> function,
+        public AsyncDebouncedAction(
+            [NotNull]Func<CancellationToken, Task> action,
             TimeSpan duration = default(TimeSpan),
             TimeSpan minimumGap = default(TimeSpan))
         {
-            Contract.Requires(function != null);
+            Contract.Requires(action != null);
             Contract.Requires(duration >= TimeSpan.Zero);
             Contract.Requires(minimumGap >= TimeSpan.Zero);
 
             // Calculate the gap ticks, based on stopwatch frequency.
             _durationTicks = duration.Ticks;
             _minimumGapTicks = minimumGap.Ticks;
-            _function = function;
+            _action = action;
         }
 
         /// <summary>
-        /// Runs the function asynchronously.
+        /// Runs the action asynchronously.
         /// </summary>
         /// <param name="token">The token.</param>
         /// <returns>An awaitable task, that completes with the debounced result.</returns>
-        /// <remarks><para>If the function is currently running, this will wait until it completes (or
+        /// <remarks><para>If the action is currently running, this will wait until it completes (or
         /// <paramref name="token"/> has been cancelled),
-        /// but won't run the function again.  However, if the original function is cancelled, or errors,
-        /// then it will run the function again immediately.</para>
+        /// but won't run the action again.  However, if the original action is cancelled, or errors,
+        /// then it will run the action again immediately.</para>
         /// </remarks>
         [NotNull]
         [PublicAPI]
-        public async Task<TResult> Run(CancellationToken token = default (CancellationToken))
+        public async Task Run(CancellationToken token = default (CancellationToken))
         {
             // Record when the request was made.
             long requested = DateTime.UtcNow.Ticks;
@@ -134,18 +129,16 @@ namespace WebApplications.Utilities.Threading
                 token.ThrowIfCancellationRequested();
 
                 if (requested <= _nextRun)
-                    return _lastResult;
+                    return;
 
                 // Await on a task.
-                TResult lastResult = await _function(token);
+                await _action(token);
 
                 // If we're cancelled we don't update our next run as we were unsuccessful.
                 token.ThrowIfCancellationRequested();
 
-                // Set the next time you can run the function.
-                _lastResult = lastResult;
+                // Set the next time you can run the action.
                 _nextRun = Math.Max(requested + _durationTicks, DateTime.UtcNow.Ticks + _minimumGapTicks);
-                return lastResult;
             }
         }
     }
