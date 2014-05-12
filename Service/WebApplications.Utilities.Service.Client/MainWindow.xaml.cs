23c25527a0976069022221d9668da146926d8140
Craig Dean <craig.dean@webappuk.com>
2014-05-12 23:20:00 +0100

    Case:32741 Added missing files!

---
+++ Service/WebApplications.Utilities.Service.Client/MainWindow.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebApplications.Utilities.Service.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public LinkedList<string> Commands = new LinkedList<string>();
+
+        public const int BufferSize = 500;
+        public const int HistorySize = 50;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the Close control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        /// <summary>
+        /// Handles the OnMouseDown event of the MainWindow control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="MouseButtonEventArgs"/> instance containing the event data.</param>
+        private void MainWindow_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the Execute control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void Execute_Click(object sender, RoutedEventArgs e)
+        {
+            string command = CommandLine.Text;
+            CommandLine.Text = string.Empty;
+            if (string.IsNullOrWhiteSpace(command))
+                return;
+            Commands.AddLast(command);
+
+            while (Commands.Count > HistorySize)
+                Commands.RemoveFirst();
+
+            AppendCommand(command);
+            // TODO Execute the command
+        }
+
+        /// <summary>
+        /// Adds the paragraph.
+        /// </summary>
+        /// <param name="paragraph">The paragraph.</param>
+        public void AppendCommand(string command)
+        {
+            AppendLine();
+            AppendText("Executed: ", Colors.Yellow, FontStyles.Italic);
+            AppendText(command, Colors.White, FontStyles.Italic);
+            AppendLine();
+        }
+
+        /// <summary>
+        /// Appends the text.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <param name="color">The color.</param>
+        public void AppendText(string text, Color color)
+        {
+            TextRange range = new TextRange(LogView.Document.ContentEnd, LogView.Document.ContentEnd) { Text = text };
+            range.ApplyPropertyValue(TextElement.ForegroundProperty, new SolidColorBrush(color));
+        }
+
+        /// <summary>
+        /// Appends the text.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <param name="color">The color.</param>
+        /// <param name="fontStyle">The font style.</param>
+        public void AppendText(string text, Color color, FontStyle fontStyle)
+        {
+            TextRange range = new TextRange(LogView.Document.ContentEnd, LogView.Document.ContentEnd) { Text = text };
+            range.ApplyPropertyValue(TextElement.ForegroundProperty, new SolidColorBrush(color));
+            range.ApplyPropertyValue(TextElement.FontStyleProperty, fontStyle);
+        }
+
+        /// <summary>
+        /// Appends the text.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <param name="fontStyle">The font style.</param>
+        public void AppendText(string text, FontStyle fontStyle)
+        {
+            TextRange range = new TextRange(LogView.Document.ContentEnd, LogView.Document.ContentEnd) { Text = text };
+            range.ApplyPropertyValue(TextElement.FontStyleProperty, fontStyle);
+        }
+
+        /// <summary>
+        /// Appends the text.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <param name="color">The color.</param>
+        /// <param name="fontWeight">The font weight.</param>
+        public void AppendText(string text, Color color, FontWeight fontWeight)
+        {
+            TextRange range = new TextRange(LogView.Document.ContentEnd, LogView.Document.ContentEnd) { Text = text };
+            range.ApplyPropertyValue(TextElement.ForegroundProperty, new SolidColorBrush(color));
+            range.ApplyPropertyValue(TextElement.FontWeightProperty, fontWeight);
+        }
+
+        /// <summary>
+        /// Appends the text.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <param name="fontWeight">The font weight.</param>
+        public void AppendText(string text, FontWeight fontWeight)
+        {
+            TextRange range = new TextRange(LogView.Document.ContentEnd, LogView.Document.ContentEnd) { Text = text };
+            range.ApplyPropertyValue(TextElement.FontWeightProperty, fontWeight);
+        }
+
+        /// <summary>
+        /// Appends the text.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <param name="color">The color.</param>
+        /// <param name="fontStyle">The font style.</param>
+        /// <param name="fontWeight">The font weight.</param>
+        public void AppendText(string text, Color color, FontStyle fontStyle, FontWeight fontWeight)
+        {
+            TextRange range = new TextRange(LogView.Document.ContentEnd, LogView.Document.ContentEnd) {Text = text};
+            range.ApplyPropertyValue(TextElement.ForegroundProperty, new SolidColorBrush(color));
+            range.ApplyPropertyValue(TextElement.FontStyleProperty, fontStyle);
+            range.ApplyPropertyValue(TextElement.FontWeightProperty, fontWeight);
+        }
+
+        /// <summary>
+        /// Appends the line.
+        /// </summary>
+        public void AppendLine()
+        {
+            LogView.AppendText(Environment.NewLine);
+            BlockCollection blocks = LogView.Document.Blocks;
+            while (blocks.Count > BufferSize)
+                blocks.Remove(blocks.FirstBlock);
+        }
+    }
+}
---
+++ Service/WebApplications.Utilities.Service.Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,63 @@
+﻿#region © Copyright Web Applications (UK) Ltd, 2014.  All rights reserved.
+// Copyright (c) 2014, Web Applications UK Ltd
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of Web Applications UK Ltd nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL WEB APPLICATIONS UK LTD BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#endregion
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+using WebApplications.Utilities;
+
+[assembly: Guid("774FD886-89B9-4DB1-8FE1-3891E735A29B")]
+[assembly: AssemblyTitle("WebApplications.Utilities.Service.Client")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("Web Applications UK Ltd")]
+[assembly: AssemblyProduct("Web Applications Service Utilities")]
+[assembly: AssemblyCopyright("Copyright © Web Applications UK Ltd, 2006-2014")]
+[assembly: AssemblyTrademark("Web Applications")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("5.0.*")]
+[assembly: AssemblySemanticVersion("{Major}.{Minor}.{Build}-RTM.{Revision}")]
+
+//In order to begin building localizable applications, set 
+//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
+//inside a <PropertyGroup>.  For example, if you are using US english
+//in your source files, set the <UICulture> to en-US.  Then uncomment
+//the NeutralResourceLanguage attribute below.  Update the "en-US" in
+//the line below to match the UICulture setting in the project file.
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+    //(used if a resource is not found in the page, 
+    // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+    //(used if a resource is not found in the page, 
+    // app, or any theme specific resource dictionaries)
+)]
