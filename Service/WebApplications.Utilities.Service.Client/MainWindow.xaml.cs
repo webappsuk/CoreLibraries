453a91987ab673c138b7447465dc886c0d5755a7
Craig Dean <craig.dean@webappuk.com>
2014-05-13 09:03:54 +0100

    Case:32741 Fixed border on Maximise.

--- Service/WebApplications.Utilities.Service.Client/MainWindow.xaml.cs
+++ Service/WebApplications.Utilities.Service.Client/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Shell;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
