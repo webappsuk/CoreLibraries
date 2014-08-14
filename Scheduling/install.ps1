7095c154fab3fe40ad666e0954b7b00de0cacda5
Craig Dean <craig.dean@webappuk.com>
2014-08-14 13:56:00 +0100

    Case:34312 Added build task to copy TimeZoneData folder to output directory.

---
+++ Scheduling/install.ps1
@@ -0,0 +1,26 @@
+param($installPath, $toolsPath, $package, $project)
+
+# This is the MSBuild targets file to add
+$targetsFile = [System.IO.Path]::Combine($toolsPath, 'Scheduling.targets')
+
+# Need to load MSBuild assembly if it's not loaded yet.
+Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+
+# Grab the loaded MSBuild project for the project
+$msbuild = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | Select-Object -First 1
+
+# Make the path to the targets file relative.
+$projectUri = new-object Uri('file://' + $project.FullName)
+$targetUri = new-object Uri('file://' + $targetsFile)
+$relativePath = $projectUri.MakeRelativeUri($targetUri).ToString().Replace([System.IO.Path]::AltDirectorySeparatorChar, [System.IO.Path]::DirectorySeparatorChar)
+
+# Remove previous imports to PerfSetup.targets
+$msbuild.Xml.Imports | Where-Object {$_.Project.ToLowerInvariant().EndsWith("scheduling.targets") } | Foreach { 
+	$_.Parent.RemoveChild( $_ ) 
+	[string]::Format( "Removed import of '{0}'" , $_.Project )
+}
+
+# Add import to PostSharp.targets
+$import = $msbuild.Xml.AddImport($relativePath)
+$import.Condition = "`$(DefineConstants.Contains('Scheduling')) AND Exists('$relativePath')"
+[string]::Format("Added import of '{0}'.", $relativePath )
