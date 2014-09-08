7fd87b0d57aee985d1d79b5358565dcd9877aa1f
Andrew Billings <andrew.billings@webappuk.com>
2014-09-08 15:05:18 +0100

    Case:16223 Fixed the un/install.ps1 scripts to properly remove the signing build target.

--- Utilities/install.ps1
+++ Utilities/install.ps1
@@ -1,7 +1,7 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
 # This is the MSBuild targets file to add
-$targetsFile = [System.IO.Path]::Combine($toolsPath, 'Web Applications Signature.targets')
+$targetsFile = [System.IO.Path]::Combine($toolsPath, 'WebApplicationsSignature.targets')
 
 # Need to load MSBuild assembly if it's not loaded yet.
 Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
@@ -12,10 +12,10 @@ $msbuild = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollecti
 # Make the path to the targets file relative.
 $projectUri = new-object Uri('file://' + $project.FullName)
 $targetUri = new-object Uri('file://' + $targetsFile)
-$relativePath = $projectUri.MakeRelativeUri($targetUri).ToString().Replace([System.IO.Path]::AltDirectorySeparatorChar, [System.IO.Path]::DirectorySeparatorChar)
+$relativePath = $projectUri.MakeRelativeUri($targetUri).ToString().Replace([System.IO.Path]::AltDirectorySeparatorChar, [System.IO.Path]::DirectorySeparatorChar).ToLowerInvariant()
 
 # Remove previous imports to targets file
-$msbuild.Xml.Imports | Where-Object {$_.Project.ToLowerInvariant().EndsWith("web applications signature.targets") } | Foreach { 
+$msbuild.Xml.Imports | Where-Object {$_.Project.EndsWith("webapplicationssignature.targets") } | Foreach { 
 	$_.Parent.RemoveChild( $_ ) 
 	[string]::Format( "Removed import of '{0}'" , $_.Project )
 }
--- Utilities/uninstall.ps1
+++ Utilities/uninstall.ps1
@@ -1,7 +1,7 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
 # This is the MSBuild targets file to remove
-$targetsFile = [System.IO.Path]::Combine($toolsPath, 'Web Applications Signature.targets')
+$targetsFile = [System.IO.Path]::Combine($toolsPath, 'WebApplicationsSignature.targets')
 
 # Need to load MSBuild assembly if it's not loaded yet.
 Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
@@ -10,7 +10,7 @@ Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, Publi
 $msbuild = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | Select-Object -First 1
 
 # Remove imports to targets file
-$msbuild.Xml.Imports | Where-Object {$_.Project.ToLowerInvariant().EndsWith("web applications signature.targets") } | Foreach { 
+$msbuild.Xml.Imports | Where-Object {$_.Project.EndsWith("webapplicationssignature.targets") } | Foreach { 
 	$_.Parent.RemoveChild( $_ ) 
 	[string]::Format( "Removed import of '{0}'" , $_.Project )
 }
