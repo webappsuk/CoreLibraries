c8de582d5d14af28798ba29e2b07827989f6cb01
Andrew Billings <andrew.billings@webappuk.com>
2015-07-25 17:04:19 +0100

    Case:16223 Changed the nuspecs to automatically add the target files to the project, so the install.ps1 and uninstall.ps1 files are no longer needed.

--- buildNugets.bat
+++ buildNugets.bat
@@ -1,8 +1,5 @@
 @echo off
 
-rem Need custom build task/script to do the package as the built in one doesnt work that well
-	rem Create .nuproj instead of .nuspec, .nuproj will output the nuspec at build to obj and package that
-
 SET slnDir=%~dp0
 
 del /S /Q "nugets\*.*" 2>nul
