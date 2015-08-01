8ef2fc00c1378cff8587f30c1059a2bb72d536d9
Craig Dean <craig.dean@webappuk.com>
2015-08-01 01:37:37 +0100

    Case:36954 Added xml documentation to nugets, to improve intellisense experience.
    Fixed buildNugets.bat to check for admin permissions and to load MSBuild environment automatically.

--- buildNugets.bat
+++ buildNugets.bat
@@ -1,8 +1,25 @@
 @echo off
 
+:: We need administrator permissions to run correctly
+net session >nul 2>&1
+if %errorLevel% == 0 (
+	echo Administrative permissions confirmed.
+) else (
+	echo Current permissions inadequate, run as administrator.
+	GOTO End
+)
+
+:: Set up VS2015 MSBuild environment.
+@call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools\VsMSBuildCmd.bat"
+cd "%~dp0"
 SET slnDir=%~dp0
 
 del /S /Q "nugets\*.*" 2>nul
 mkdir nugets 2>nul
 
 msbuild Core-Libraries.sln /p:Configuration=NuGet;SolutionDir=%slnDir% /t:rebuild
+
+
+:End
+echo.
+pause >nul
